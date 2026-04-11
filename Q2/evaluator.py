25f49f3a83b2cdce4822178b61e4f5e29e17e69b
LW2511 <lincoln.whitehead@students.cdu.edu.au>
2026-04-11 11:36:49 +1000

    Update evaluator.py

---
+++ Q2/evaluator.py
@@ -0,0 +1,8 @@
+#Step 1: Read input file
+def evaluate_file(input_path: str):
+  with open(input_path, "r") as file:
+    lines = f.readlines()
+ for line in lines:
+        print(line.strip())
+
+#Step 2: Tokenizer Converting string into tokens 
